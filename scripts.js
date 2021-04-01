f013decc66f5e4a9d869ab38c5c79846ce54a302
kelvin012 <k20kelvin20@hotmail.com>
2021-04-01 03:06:25 -0300

    :tada: aula 04

--- scripts.js
+++ scripts.js
@@ -99,6 +99,7 @@ const Utils = {
 
         return Math.round(value)
     },
+
     formatDate(date) {
         const splittedDate = date.split('-')
         return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
